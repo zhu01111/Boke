02242ca0e833175d9f3fbb440d0241cf4db877d3
zhu01111 <39577199+zhu01111@users.noreply.github.com>
2020-07-23 18:10:43 +0800

    Update SmsUtils.java
    qq

--- src/main/java/com/zwc/Boke/util/SmsUtils.java
+++ src/main/java/com/zwc/Boke/util/SmsUtils.java
@@ -22,12 +22,12 @@ public class SmsUtils{
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAIGlFgnAeAhi4M";
-    static final String accessKeySecret = "vd48YjYRu3XAlzLiDTpqnMjZw31bM8";
+    static final String accessKeyId = "";
+    static final String accessKeySecret = "";
     
-    static final String smsTemplateAddress = "SMS_140550942";
+    static final String smsTemplateAddress = "";
     
-    static final String signName = "朱伟超";
+    static final String signName = "zhuwc";
     
     
     public static void main(String[] args){
